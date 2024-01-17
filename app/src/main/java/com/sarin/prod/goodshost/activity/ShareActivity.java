8ef39b16c6be87539c1f60236867052fecdd97a7
hks1984 <soir1984@gmail.com>
2024-01-17 18:40:03 +0900

    쿠팡 공유 받기 기능 추가중.

---
+++ app/src/main/java/com/sarin/prod/goodshost/activity/ShareActivity.java
@@ -0,0 +1,16 @@
+package com.sarin.prod.goodshost.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sarin.prod.goodshost.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
