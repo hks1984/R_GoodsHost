0f8a304ca02a84f7fdcd083526ffbab73489ef12
hks1984 <soir1984@gmail.com>
2024-02-05 02:28:52 +0900

    설정화면

---
+++ app/src/main/java/com/sarin/prod/goodshost/fragment/setting/SettingViewModel.java
@@ -0,0 +1,7 @@
+package com.sarin.prod.goodshost.fragment.setting;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
