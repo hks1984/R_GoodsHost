7b827f3c17e6da893ade28c87991bb8c73afd307
hks1984 <soir1984@gmail.com>
2024-06-18 03:49:41 +0900

    카테고리 클릭이벤트 수정. 문제: 어댑터에서 엑티비티의 retrofit 호출시 로딩 처리 시 널포인트 오류 발생됨.

---
+++ app/src/main/java/com/sarin/prod/goodshost/adapter/CateRecyclerViewClickListener.java
@@ -0,0 +1,8 @@
+package com.sarin.prod.goodshost.adapter;
+
+import android.view.View;
+
+public interface RecyclerViewClickListener {
+    void onItemClickListener(View v, int position);
+    void onItemClickListener_Hori(View v, int position);
+}
